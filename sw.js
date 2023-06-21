8ce5a8b26840aea41130fd67e51d107c3d241852
rupertiexplorer <ruperti.explorer@gmail.com>
2023-06-21 12:49:18 +0200

    -began implementing a service worker -updated gallery with Bio,chem,phy,art,sport entries

---
+++ sw.js
@@ -0,0 +1,51 @@
+//by https://www.youtube.com/TheNetNinja
+const staticCacheName = 'site-static-v4';
+const dynamicCacheName = 'site-dynamic-v4';
+const assets = [
+  '/',
+  '/index.html',
+  '/code.html',
+  '/galerie.html',
+  '/assets/js/app.js',
+  '/assets/js/main.js',
+  '/assets/css/main.css',
+  '/assets/css/noscript.css',
+  '/assets/images/dish.png',
+  '/pages/fallback.html'
+];
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
+// install event
+self.addEventListener('install', evt => {
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  //console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      //console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
