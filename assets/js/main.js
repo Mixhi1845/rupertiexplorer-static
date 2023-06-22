fc203a80665594a930a6683b7896faa40e629aba
rupertiexplorer <ruperti.explorer@gmail.com>
2023-06-22 09:23:13 +0200

    -updated search codes -added flyer to karte.html -updated gallery article arrangement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,20 @@
 const codeToPage = {
-  'edv': '/galerie/edv.html',
-  'sportplatz': '/galerie/sportplatz.html',
-  'mensa': '/galerie/mensa.html',
-  'kleine Pausenhalle': '/galerie/-.html',
-  'große Pausenhalle': '/galerie/-.html',
-  'archiv': '/galerie/archiv.html'
+  '11': '/galerie/ankunft.html',
+  '21': '/galerie/sportplatz.html',
+  '22': '/galerie/sporthallen.html',
+  '23': '/galerie/sprachenraum.html',
+  '24': '/galerie/kunstsäle.html',
+  '31': '/galerie/informatik.html',
+  '32': '/galerie/chemie.html',
+  '33': '/galerie/physik.html',
+  '34': '/galerie/biologie.html',
+  '41': '/galerie/seketariat.html',
+  '42': '/galerie/bibliothek.html',
+  '43': '/galerie/lehrerzimmer.html',
+  '51': '/galerie/mensa.html',
+  '52': '/galerie/gPausenhalle.html',
+  '53': '/galerie/kPausenhalle.html',
+  '61': '/galerie/archiv.html',
 };
 
 const codeInput = document.getElementById('code-input');
